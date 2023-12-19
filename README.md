2d48da762cfc94355bce4d1d2c830fc46c42a60d
ozlem <06ozlemozturk@gmail.com>
2023-12-19 21:42:33 +0300

    node

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 03-NodeJS
+
+- create index.js
+
+- $ npm init -y
+- create .gitignore (copy from gitignore.io)
---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict ";
+
+/* ----------------------------------------
+
+          NODEJS
+
+---------------------------------------- */
+
+const htpp = require("node:http"); // builtin: https://nodejs.org/api/http.html
+const { request } = require("node:https");
+
+//http.createServer((req, res) => { ... }
+http.createServer((request, response) => {});
+/* ----------------------------------------*/
