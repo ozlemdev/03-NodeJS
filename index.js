03b284aeb7ba275fe01da879b9cf8d9b5a41190b
ozlem <06ozlemozturk@gmail.com>
2023-12-19 23:15:09 +0300

    node

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 
 -------------------------------------------- */
 const http = require("node:http"); // builtin: https://nodejs.org/api/http.html
-/* -------------------------------------------- */
+/* -------------------------------------------- 
 
 // http.createServer((req, res) => { ... }
 const app = http.createServer((request, response) => {
@@ -18,3 +18,8 @@ const app = http.createServer((request, response) => {
 // http://127.0.0.1:8000
 // http://localhost:8000
 app.listen(8000, () => console.log("Server Runned: http://127.0.0.1:8000"));
+/* -------------------------------------------- */
+
+const app = http
+  .createServer((req, res) => {})
+  .listen(8000, () => console.log("Server Runned: http://127.0.0.1:8000"));
