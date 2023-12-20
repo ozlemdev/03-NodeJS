0d48852e6060cb6d674293e453be022e0516fd2a
ozlem <06ozlemozturk@gmail.com>
2023-12-20 21:45:42 +0300

    cmmt

--- index.js
+++ index.js
@@ -44,12 +44,35 @@ http
       res.statusMessage = "Not Found"; // Default: OK
 
       res.setHeader("Content-Type", "text/html");
-      res.setHeader("anather-header", "another-value");
+      res.setHeader("another-header", "another-value");
 
       res.write("* Satır1");
       res.write("* Satır2");
       res.write("* Satır3");
       res.end();
+    } else if (req.url == "/api") {
+      if (req.method == "GET") {
+        // res.writeHead(200, {
+        //     'Content-Type': 'application/json',
+        //     'another-header': 'another-value',
+        // })
+
+        // OVERLOAD GOOD EXAMPLE:
+        res.writeHead(200, "Status Message", {
+          //kısa yol kullanımı
+          "Content-Type": "application/json",
+          "another-header": "another-value",
+        });
+
+        const obj = {
+          result: true,
+          message: "İşlem Başarılı.",
+        };
+
+        res.end(JSON.stringify(obj));
+      } else {
+        res.end("Not supporting different method.");
+      }
     } else {
       res.end("Server is running");
     }
